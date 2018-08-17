8af14d3802eef02d4ab381f2b55447f24527680a
AnnaKhariv <khariv.anna@gmail.com>
2018-08-17 19:34:56 +0200

    FE_9_6_homework_js-intro done!

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -2,16 +2,16 @@ let price = Number(prompt('Enter amount of money: ', '0'));
 let discount = Number(prompt('Enter discount: ', '0'));
 let result;
 
-if(price < 0 || discount < 0 || discount > 100 || isNaN(price) || isNaN(discount)) {
+if (price < 0 || discount < 0 || discount > 100 || isNaN(price) || isNaN(discount)) {
     result = 'Invalid data';
 } else {
-    let saved = price * discount/100;
+    let saved = price * discount / 100;
     let priceWihDiscount = price - saved;
     result = `
-        Price without discount:  ${+price}\n 
-        Discount:                ${+discount}%\n
-        Price with discount:     ${+priceWihDiscount}\n
-        Saved:                   ${+saved}`;
+        Price without discount:  ${+price.toFixed(2)}\n 
+        Discount:                ${+discount.toFixed(2)}%\n
+        Price with discount:     ${+priceWihDiscount.toFixed(2)}\n
+        Saved:                   ${+saved.toFixed(2)}`;
 }
 
 console.log(result);
--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -1,1 +1,26 @@
-// Your code goes here
+let sideA = Number(prompt('Enter side A length: ', '0'));
+let sideB = Number(prompt('Enter side B length: ', '0'));
+let angle = Number(prompt('Enter angle between sides: ', '0'));
+let result;
+
+if (isValid(sideA) || isValid(sideB) || isValid(angle)) {
+    result = 'Invalid data';
+} else {
+    const DEGREE = 180;
+    let angleRadian = angle * Math.PI / DEGREE;
+
+    let sideC = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2) - 2 * sideA * sideB * Math.cos(angleRadian));
+    let triangleSquare = 1/2 * sideA * sideB * Math.sin(angleRadian);
+    let trianglePerimeter = sideA + sideB + sideC;
+
+    result = `
+        С length:           ${+sideC.toFixed(2)}\n
+        Triangle square:    ${+triangleSquare.toFixed(2)}\n              
+        Triangle perimeter: ${+trianglePerimeter.toFixed(2)}`;
+}
+
+function isValid(value) {
+    return isNaN(value) || value.toFixed(2) <= 0;
+}
+
+console.log(result);
